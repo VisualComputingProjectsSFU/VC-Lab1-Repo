9629f45c820679f0b2c304e4a301e8c13f66a0d1
Wei Wang <me@mrwei.wang>
2018-09-19 22:54:04 -0700

    Add files via upload
    Implemented input image cropping and resizing. Preview code is also added to the main function.

--- dataset.py
+++ dataset.py
@@ -3,11 +3,13 @@ import numpy as np
 import os
 import main
 from torch.utils.data import Dataset
+import PIL
 from PIL import Image
 
 
 class AlexNetDataset(Dataset):
     img_w, img_h = 225, 225
+    dir_name_trim_length = -9
 
     def __init__(self, data_list):
         self.data_list = data_list
@@ -18,58 +20,63 @@ class AlexNetDataset(Dataset):
     def __getitem__(self, idx):
         item = self.data_list[idx]
 
-        self.file_path = os.path.join(main.lfw_dataset_path, item['file_path'])
+        file_name = item['file_path']
+        self.file_path = os.path.join(main.lfw_dataset_path, file_name[:self.dir_name_trim_length], file_name)
         self.crops = item['crops'].split(' ')
         self.landmarks = item['landmarks'].split(' ')
 
-        img_tensor = np.asarray(Image.open(self.file_path), dtype=np.float32)
+        # Prepare image tensor.
+        img_tensor = Image.open(self.file_path)
         img_tensor = self.crop(img_tensor)
         img_tensor = self.flip(img_tensor)
         img_tensor = self.brighten(img_tensor)
         img_tensor = self.normalize(img_tensor)
 
-        landmarks = np.asarray(self.landmarks)
-        landmark_tensors = torch.from_numpy(landmarks).long()
+        # Prepare landmark tensor.
+        landmarks = np.asarray(list(map(float, self.landmarks)))
+        landmarks = landmarks.reshape(7, 2)
+        landmarks = self.crop(landmarks)
+        landmark_tensors = torch.from_numpy(landmarks)
 
         # 225 x 225 x 3 input image tensor. 7 * 2 landmark tensor.
         return img_tensor, landmark_tensors
 
-    def preview(self, idx=-1, is_landmark_displayed=True):
-        if idx == -1:
-            # Random select a preview image.
-            # idx = random.random within dataset range
-            pass
+    def crop(self, _input):
+        # Case that a image need to be cropped.
+        if isinstance(_input, PIL.JpegImagePlugin.JpegImageFile):
+            img = _input
+            img = img.crop(list(map(int, self.crops)))
+            img = img.resize((self.img_w, self.img_h), PIL.Image.ANTIALIAS)
+            img = np.asarray(img)
 
-        if is_landmark_displayed:
-            # Display landmarks on the preview image.
-            pass
-        else:
-            # Do not display landmarks on the preview image.
-            pass
+            h, w = img.shape[0], img.shape[1]
+            chanel = img.shape[2]
 
-        return self.__getitem__(idx)
+            # Create image tensor
+            img_tensor = torch.from_numpy(img)
 
-    def crop(self, original_image):
-        img = original_image
+            # Reshape to (3, 225, 225), the 1 is the channel size
+            img_tensor = img_tensor.view((chanel, h, w))
+            return img_tensor
 
-        h, w = img.shape[0], img.shape[1]
-        chanel = img.shape[2]
-
-        # Create image tensor
-        img_tensor = torch.from_numpy(img)
-
-        # Reshape to (1, 28, 28), the 1 is the channel size
-        img_tensor = img_tensor.view((chanel, h, w))
-        return img_tensor
+        # Case that the landmarks need to be adjusted after cropping.
+        landmarks = _input
+        shift = np.array([[self.crops[0], self.crops[1]]])
+        shift = np.repeat(shift, 7, axis=0)
+        ratio = float(self.img_w) / (float(self.crops[2]) - float(self.crops[0]))
+        return np.subtract(landmarks.astype(float), shift.astype(float)) * ratio
 
     @staticmethod
     def flip(cropped_img):
-        return cropped_img
+        img = cropped_img
+        return img
 
     @staticmethod
     def brighten(cropped_img):
-        return cropped_img
+        img = cropped_img
+        return img
 
     @staticmethod
     def normalize(cropped_img):
-        return cropped_img
+        img = cropped_img
+        return img
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import random
+import torch.utils.data
+import dataset
+import matplotlib.pyplot as plt
+
 
 lfw_dataset_path = 'lfw'
 test_landmark_path = os.path.join(lfw_dataset_path, 'LFW_annotation_test.txt')
@@ -9,15 +13,18 @@ training_ratio = 0.8
 if __name__ == '__main__':
     training_validation_data_list = []
     testing_data_list = []
+
+    # Read training and validation data.
     with open(train_landmark_path, "r") as file:
         for line in file:
             tokens = line.split('\t')
             if len(tokens) == 3:
                 file_path = tokens[0]
-                crops = tokens[1]
+                crops = ' '.join(tokens[1].split())
                 landmarks = ' '.join(tokens[2].split())
                 training_validation_data_list.append({'file_path': file_path, 'crops': crops, 'landmarks': landmarks})
 
+    # Read testing data.
     with open(test_landmark_path, "r") as file:
         for line in file:
             tokens = line.split('\t')
@@ -41,3 +48,35 @@ if __name__ == '__main__':
 
     # Testing dataset
     test_set_list = testing_data_list
+
+    train_dataset = dataset.AlexNetDataset(train_set_list)
+    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=6)
+    print('Total training items', len(train_dataset), ', Total training batches per epoch:', len(train_data_loader))
+
+    valid_set = dataset.AlexNetDataset(valid_set_list)
+    valid_data_loader = torch.utils.data.DataLoader(valid_set, batch_size=32, shuffle=True, num_workers=6)
+    print('Total validation set:', len(valid_set))
+
+    '''
+        Preview random cropped images.
+    '''
+    idx, (image, landmarks) = next(enumerate(train_data_loader))
+    print('Image tensor shape (N, C, H, W):', image.shape)
+    print('Label tensor shape (N, X, Y):', landmarks.shape)
+
+    n_batch_size = image.shape[0]
+    channels = image.shape[1]
+    h, w = image.shape[2], image.shape[3]
+
+    nd_img = image.cpu().numpy()
+    nd_landmarks = landmarks.cpu().numpy()
+
+    plt.imshow(nd_img[0].reshape(h, w, 3))
+    plt.plot(nd_landmarks[0][0][0], nd_landmarks[0][0][1], 'bo')
+    plt.plot(nd_landmarks[0][1][0], nd_landmarks[0][1][1], 'bo')
+    plt.plot(nd_landmarks[0][2][0], nd_landmarks[0][2][1], 'bo')
+    plt.plot(nd_landmarks[0][3][0], nd_landmarks[0][3][1], 'bo')
+    plt.plot(nd_landmarks[0][4][0], nd_landmarks[0][4][1], 'bo')
+    plt.plot(nd_landmarks[0][5][0], nd_landmarks[0][5][1], 'bo')
+    plt.plot(nd_landmarks[0][6][0], nd_landmarks[0][6][1], 'bo')
+    plt.show()
