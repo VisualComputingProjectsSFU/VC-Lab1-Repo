50142339c191eb20c5fbbd2514293429bd17986e
Vishnu Sanjay Ramiya Srinivasan <vishnusanjay.rs@gmail.com>
2018-09-26 15:38:01 -0700

    Add files via upload

--- vishnu/facial_test1.py
+++ vishnu/facial_test1.py
@@ -12,6 +12,8 @@ import random
 from random import randint
 from torch.autograd import Variable
 
+n_detection_range = 100
+
 if __name__ == '__main__':
     lfw_dataset_path = '/home/vishnusanjay/PycharmProjects/FacialReg/lfw'
     test_landmark_path = os.path.join(lfw_dataset_path, 'LFW_annotation_test.txt')
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     test_set_list = testing_data_list
 
     test_dataset = dataset.LFWDataset(test_set_list)
-    test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=0)
+    test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=True, num_workers=0)
     print('Total teting items', len(test_dataset), ', Total training batches per epoch:', len(test_data_loader))
 
     test_net = LFWNet.LfwNet()
@@ -42,23 +44,45 @@ if __name__ == '__main__':
     test_net_state = torch.load(os.path.join(lfw_dataset_path, 'lfwnet_1.pth'))
 
     test_net.load_state_dict(test_net_state)
-    print(testing_data_list[0])
 
-    test_landmarks = []
-    pred_landmarks = []
-    itr = 0
-
-    for idx in range(0, len(test_data_loader)):
-        test_idx, (test_input, test_landmark) = next(enumerate(test_data_loader))
+    # Generate predictions.
+    loss_raw = []
+    for test_batch_idx, (test_input, test_oracle) in enumerate(test_data_loader):
         test_net.eval()
-        pred_landmark = test_net.forward(test_input)
-        test_array = np.array(test_landmark)
-        test_landmarks.append(test_array)
-        pred_array = pred_landmark.detach()
-        pred_array =np.array(pred_array)
-        pred_landmarks.append(pred_array)
+        test_net.cuda()
+        predictions = test_net.forward(test_input.cuda())
 
-    test_landmarks_arr= np.array(test_landmarks)
-    pred_landmarks_arr = np.array(pred_landmarks)
-    print(test_landmarks_arr.shape)
-    print(pred_landmarks_arr.shape)
+        # Compute overall loss.
+        batch_loss = np.linalg.norm(np.array(predictions.detach()) - np.array(test_oracle.detach()), axis=1)
+        print(np.array(predictions.detach()).shape)
+        print(np.array(test_oracle.detach()).shape)
+        print(batch_loss.shape)
+        batch_loss *= 225
+        loss_raw.extend(batch_loss.tolist())
+
+
+
+    # Compute loss plot.
+    loss_raw = np.array(loss_raw).flatten()
+    print(loss_raw.shape)
+    loss_plot = []
+    for step in range(1, n_detection_range):
+        loss_plot.append((step, len(np.where(loss_raw < step)[0]) / float(len(loss_raw))))
+        print(len(np.where(loss_raw < step)[0]))
+        print(len(loss_raw))
+        print(len(np.where(loss_raw < step)[0]) / len(loss_raw))
+    loss_plot = np.asarray(loss_plot,dtype=np.float32)
+    print(loss_plot)
+    plt.figure(num='Percentage of Detected Key-points')
+    plt.title('Percentage of Detected Key-points')
+    plt.xlabel('L2 Distance From Detected Points to Ground Truth Points')
+    plt.ylabel('Percentage')
+    axes = plt.gca()
+    axes.set_xticks(np.arange(0, n_detection_range, 5))
+    axes.set_yticks(np.arange(-0.1, 1.1, 0.1))
+    axes.set_xlim([0, n_detection_range])
+    axes.set_ylim([-0.05, 1.05])
+    plt.grid()
+    plt.plot(loss_plot[:, 0], loss_plot[:, 1])
+
+    plt.show()
